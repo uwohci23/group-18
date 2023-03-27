de01fabcd360b2ec2f863ade1dbd7dfefff08666
Theodore Forcier <tforcie@uwo.ca>
2023-03-27 01:23:09 -0400

    Difficulties work

--- Minesweeper/minesweeper.js
+++ Minesweeper/minesweeper.js
@@ -1,11 +1,33 @@
 import { TILE_STATUS, createBoard, flagTile, revealTile, checkWin, checkLose } from "./game.js"
 import { stop } from "./timer.js"
 
-const BOARD_HEIGHT = 8
-const BOARD_WIDTH = 8
-const NUMBER_OF_MINES = 4
+// Retrieving difficulty and adjusting board parameters
+const difficulty = localStorage.getItem('ms-difficulty')
+var boardHeight, boardWidth, numMines
+switch (difficulty) {
+  case 'easy':
+    boardHeight = 10
+    boardWidth = 10
+    numMines = 10
+    break
+  case 'medium':
+    boardHeight = 14
+    boardWidth = 18
+    numMines = 40
+    break
+  case 'hard':
+    boardHeight = 20
+    boardWidth = 24
+    numMines = 99
+    break
+  default:
+    boardHeight = 8
+    boardWidth = 8
+    numMines = 4
+    break
+}
 
-const board = createBoard(BOARD_HEIGHT, BOARD_WIDTH, NUMBER_OF_MINES)
+const board = createBoard(boardHeight, boardWidth, numMines)
 const boardElement = document.querySelector('.board')
 const minesLeftText = document.querySelector('[data-mine-count]')
 const messageText = document.querySelector('.subtext')
@@ -24,32 +46,26 @@ board.forEach(row => (
     })
   })
 ))
-boardElement.style.setProperty('--width', BOARD_WIDTH)
-boardElement.style.setProperty('--height', BOARD_HEIGHT)
-minesLeftText.textContent = NUMBER_OF_MINES
+boardElement.style.setProperty('--width', boardWidth)
+boardElement.style.setProperty('--height', boardHeight)
+minesLeftText.textContent = numMines
 
 function listMinesLeft() {
   const flaggedCount = board.reduce((count, row) => {
     return count + row.filter(tile => tile.status === TILE_STATUS.MARKED).length
   }, 0)
 
-  minesLeftText.textContent = NUMBER_OF_MINES - flaggedCount 
+  minesLeftText.textContent = numMines - flaggedCount 
 }
 
 function checkGameEnd() {
   const win = checkWin(board)
   const lose = checkLose(board)
 
-  if (win || lose) {
-    stop()
-    boardElement.addEventListener('click', stopProp, { capture: true })
-    boardElement.addEventListener('contextmenu', stopProp, { capture: true })
-  }
-
   if (win) {
     messageText.textContent = "You Win!"
   }
-  if (lose) {
+  else if (lose) {
     messageText.textContent = "You Lose"
     board.forEach(row => {
       row.forEach(tile => {
@@ -57,6 +73,12 @@ function checkGameEnd() {
         if (tile.mine) revealTile(board, tile)
       })
     })
+  }
+
+  if (win || lose) {
+    boardElement.addEventListener('click', stopProp, { capture: true })
+    boardElement.addEventListener('contextmenu', stopProp, { capture: true })
+    stop()
   }
 }
 
