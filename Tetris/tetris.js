296bf1c5e18c8d4ff33f1607d36353d0cf417732
Kashawn-Brown <kashawnbrown@hotmail.com>
2023-03-29 02:28:49 -0400

    Score on Game Over
    Added information displayed upon Game Over. Still need Game Over Screen for input and High Scores

--- Tetris/tetris.js
+++ Tetris/tetris.js
@@ -377,14 +377,18 @@ function getRandomInt2(min, max) {
   
     context.fillStyle = 'black';
     context.globalAlpha = 0.75;
-    context.fillRect(0, canvas.height / 2 - 30, canvas.width, 60);
+    context.fillRect(0, canvas.height / 2 - 85, canvas.width, 160);
   
     context.globalAlpha = 1;
     context.fillStyle = 'white';
     context.font = '36px monospace';
     context.textAlign = 'center';
     context.textBaseline = 'middle';
-    context.fillText('GAME OVER!', canvas.width / 2, canvas.height / 2);
+    context.fillText("GAME OVER!", canvas.width / 2, canvas.height / 2 - 60);
+    context.font = '32px monospace';
+    context.fillText("Your Score:", canvas.width / 2, canvas.height / 2);
+    context.font = '32px monospace bold';
+    context.fillText((" " + score + " "), canvas.width / 2, canvas.height / 2 + 45);
   }
   
   const canvas = document.getElementById('game');
