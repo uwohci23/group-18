ab09e48152428bfa19f019be08d6f9eb09ad4bcf
Kashawn-Brown <kashawnbrown@hotmail.com>
2023-03-31 02:58:11 -0400

    testing
    Tested changing gameboard color

--- Tetris/tetris.js
+++ Tetris/tetris.js
@@ -530,6 +530,7 @@ function getRandomInt2(min, max) {
   }
   // changeBackgroundColor("gameScreen", "Blue")
   // changeBackgroundColor("body", "Red")
+  //changeBackgroundColor("game", "gray")
 
   function whiteTheme()
   {
