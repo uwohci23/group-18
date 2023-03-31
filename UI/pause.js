965c5aa788ad5b3e66615cb7aee518ea2f79b4a2
Michael Dias <81830128+MichaelDias8@users.noreply.github.com>
2023-03-31 18:29:03 -0400

    Co-authored-by: Rahulj30 <Rahulj30@users.noreply.github.com>

--- UI/pause.js
+++ UI/pause.js
@@ -6,6 +6,10 @@ const closeModalButtons = document.querySelectorAll('[data-modal-close]'); //res
 const restartButton = document.getElementById('restart'); //restart
 const menuButton = document.getElementById('menu'); //menu
 const overlay = document.getElementById('overlay'); //screen  
+const confirmationPanel = document.querySelector('.confirmation-panel');
+const confirmBtn = document.querySelector('.confirm-btn');
+const cancelBtn = document.querySelector('.cancel-btn');
+
 console.log(openModalButtons)
 
 // Open pause events
@@ -33,13 +37,40 @@ closeModalButtons.forEach(button => {
 
 // Restart event
 restartButton.addEventListener('click', () => {
-  location.reload();
+  confirmationPanel.style.display = 'flex';
+  document.querySelector(".pause-modal").classList.remove('active');
+  confirmationPanel.classList.add('restart-cmd');
 })
 
 // Menu event
 menuButton.addEventListener('click', () => {
-  location.href = "../UI/game-select.html";
+  confirmationPanel.style.display = 'flex';
+  document.querySelector(".pause-modal").classList.remove('active');
 })
+
+confirmBtn.addEventListener('click', function() {
+  // Code to confirm goes here
+  if(confirmationPanel.classList.contains('restart-cmd')) {
+      location.reload();
+      confirmationPanel.classList.remove('restart-cmd');
+  }else{
+      location.href = "../UI/game-select.html";
+  }
+  // Hide the confirmation panel
+  confirmationPanel.style.display = 'none';
+});
+
+// Add event listener to cancel button
+cancelBtn.addEventListener('click', function() {
+  // Code to cancel goes here
+  confirmationPanel.classList.remove('restart-cmd');
+  if(confirmationPanel.classList.contains('end'))
+      document.querySelector(".game-over-modal").classList.add('active');
+  else
+      document.querySelector(".pause-modal").classList.add('active');
+  // Hide the confirmation panel
+  confirmationPanel.style.display = 'none';
+});
 
 function openModal(modal) {
   if(!overlay.classList.contains("active")){
