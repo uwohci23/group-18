48c8bdad7d036432d73eb28eed342dbb1d0df435
Theodore Forcier <tforcie@uwo.ca>
2023-04-05 20:01:26 -0400

    design-principle-switch

--- Report/app.js
+++ Report/app.js
@@ -1,3 +1,4 @@
+// Animations
 let options = {
   threshold: 0,
 };
@@ -9,3 +10,25 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+// Design Principles Loading
+function loadHtml(id, filename) {
+  console.log(`div id: ${id}, filename: ${filename}`);
+
+  let xhttp;
+  let element = document.getElementById(id);
+  let file = filename;
+
+  if (file) {
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4) {
+        if (this.status == 200) {element.innerHTML = this.responseText;}
+        if (this.status == 404) {element.innerHTML = "<h1>PAGE NOT FOUND</h1>";}
+      }
+    }
+    xhttp.open("GET", `Principles/${file}`, true);
+    xhttp.send();
+    return;
+  }
+}
